fb6a2cae6a7572d2d9c9c7fd80d0d4d7b49a09b5
Kwezi-Njikelana <kwezinjiks@gmail.com>
2026-06-06 19:09:13 +0200

    fix: error on logs;

--- server/src/index.js
+++ server/src/index.js
@@ -24,6 +24,6 @@ app.get('/api/health', (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
